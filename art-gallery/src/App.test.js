03031a76e72358bcbc62e276e120082cd9ee8c8a
Thiago Melonzini <thiago.melonzini@gmail.com>
2025-04-22 18:14:38 +1000

    Delete art-gallery directory

--- art-gallery/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import React from 'react';
-
-test('test the home text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/home/i);
-  expect(linkElement).toBeInTheDocument();
-});
